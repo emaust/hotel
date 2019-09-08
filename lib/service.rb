9a7b81fe41a827ac0ca22461f6931babde096e26
emaust <erikamaust@gmail.com>
2019-09-07 23:59:48 -0700

    Creates accurate instance of cost: passed

--- lib/service.rb
+++ lib/service.rb
@@ -7,7 +7,7 @@ class Service
   attr_reader :rooms, :price, :check_in, :check_out
   attr_accessor :reservation, :cost, :passenger
   
-  def initialize(check_in, check_out)
+  def initialize
     @rooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
     @cost = cost
     @price = 200
@@ -16,9 +16,8 @@ class Service
   end
   
   
-  
   def room_cost(duration)
-    @cost = duration * price 
+    @cost = (duration - 1) * price 
   end
 end
 
--- test/service_test.rb
+++ test/service_test.rb
@@ -5,18 +5,24 @@ require_relative '../lib/time'
 describe "Room functions" do
   it "lists the rooms within the hotel" do
     
-    reservation = Service.new(2010/12/20, 2010/12/26)
+    reservation = Service.new
     expect(reservation.rooms).must_be_kind_of Array
   end
   
   it "accurately returns a room number from array" do
-    reservation = Service.new(2010/12/20, 2010/12/26)
+    reservation = Service.new
     expect(reservation.rooms[4]).must_equal 5 
   end
   
-  it "creates an instance of duration" do
+  it "creates an accurate instance of duration" do
     @duration = Dates.new.reserve_time("2010/12/20", "2010/12/26")
     expect(@duration).must_equal 6
+  end
+  
+  it "creates an accurate instance of cost" do
+    @duration = Dates.new.reserve_time("2010/12/20", "2010/12/26")
+    @cost = Service.new.room_cost(@duration)
+    expect(@cost).must_equal 1000 
   end
 end 
 # describe "makes reservation" do
