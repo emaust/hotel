213f5ca0b15b6c260c444c2f88f1673c28e73cfb
emaust <erikamaust@gmail.com>
2019-09-23 18:13:38 -0700

    Updated all tests w/ copy/paste from specs folder

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -1,47 +1,12 @@
+require_relative "test_helper"
 
-require_relative 'test_helper'
-require_relative '../lib/time'
-require_relative '../lib/service'
-require_relative '../lib/reservation'
-
-
-describe "Reservation class" do
-  it "initializes Reservation" do
-    @reservation = Reservation.new("2018/12/20", "2018/12/26")
-    expect(@reservation).must_be_kind_of Reservation
+describe Hotel::Reservation do
+  describe "cost" do
+    it "returns a number" do
+      start_date = Date.new(2017, 01, 01)
+      end_date = start_date + 3
+      reservation = Hotel::Reservation.new(start_date, end_date, nil)
+      expect(reservation.cost).must_be_kind_of Numeric
+    end
   end
-  
-  it "creates an instance of check_in" do
-    @reservation = Reservation.new("2018/12/20", "2018/12/26")
-    expect(@reservation.check_in).must_equal "2018/12/20"
-  end
-  
-  it "creates an instance of check_out" do
-    @reservation = Reservation.new("2018/12/20", "2018/12/26")
-    expect(@reservation.check_out).must_equal "2018/12/26"
-  end
-  
-  it "creates a reservation with correct duration" do
-    reservation = Reservation.new("2000/10/10","2000/10/15")
-    expect(reservation.duration).must_equal 5
-  end
-  
-  it "creates a reservation with correct cost" do
-    reservation = Reservation.new("2000/10/10","2000/10/15")
-    expect(reservation.cost).must_equal 800
-  end
-  
-  it "assignes a room number" do
-    reservation = Reservation.new("2000/10/10","2000/10/15")
-    expect(reservation.room).must_be_kind_of Integer
-  end
-  
-  
-  
-  
 end
-# describe "populates reservation array" do
-#   it "adds a reservation to reservations" do
-#     Service.new.make_reservation("2010/12/20", "2010/12/26")
-#     expect(@reservations.length).must_equal 1
-#   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,15 +2,13 @@
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
-require 'simplecov'
+require "minitest/skip_dsl"
 
-# SimpleCov.start do 
-#   add_filter 'test/'
-# end
+require "date"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-
-require_relative '../lib/hotel_controller'
-require_relative '../lib/reservation'
-require_relative '../lib/date_range'
+# Require_relative your lib files here!
+require_relative "../lib/hotel_controller.rb"
+require_relative "../lib/reservation.rb"
+require_relative "../lib/date_range.rb"
