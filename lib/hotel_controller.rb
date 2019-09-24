d866cee9c061b33460a835cd0366f01ea378b1c5
emaust <erikamaust@gmail.com>
2019-09-24 02:18:37 -0700

    Created reservation_list instance variable, implemented reservations method

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -5,10 +5,13 @@ module Hotel
   class HotelController 
     
     attr_reader :rooms
+    # does this need to be accessor? idk, look it up
+    attr_accessor :reservation_list
     
     # Wave 1
     def initialize
       @rooms = [(1..20)]
+      @reservation_list = []
     end
     
     def reserve_room(start_date, end_date)      
@@ -18,12 +21,19 @@ module Hotel
       end_date = range.end_date
       nights = range.nights
       
-      return Reservation.new(start_date, end_date, nights, nil)
+      reservation = Reservation.new(start_date, end_date, nights, nil)
+      return reservation
+      @reservation_list << reservation
       
     end
     
+    # possible trickery, reassess when more reservations are generated?
     def reservations(date)
-      return []
+      reservation_list.each do |index, date|
+        if reservation_list[index].include?(date)
+          print reservation_list[index]
+        end 
+      end
     end
     
     # Wave 2
